cd880455453fd6bcd7d17d652365cc9b7365d3b9
Osirus <methanblun@gmail.com>
2024-09-25 20:17:39 +0000

    displaying score depending on listOfNum array

--- logic.js
+++ logic.js
@@ -1,14 +1,12 @@
 // TODO: faire en sorte que sameNumber ne contienne pas de doublons
 // TODO: faire une fonction qui verifie le score du joueur et l'affiche
 
-
-
 const playBtn = document.getElementById("play-button");
 const result = document.getElementById("info-display");
 let entries = document.getElementById("user-inputnum").value;
 
 function listComparison(listA, listB) {
-  let sameNumber = []
+  let sameNumber = [];
   for (let i = 0; i < listA.length; i++) {
     for (let j = 0; j < listB.length; j++) {
       if (listA[i] === listB[j]) {
@@ -16,16 +14,14 @@ function listComparison(listA, listB) {
       }
     }
   }
-  console.log(Array.from(new Set(sameNumber)))
+alert("votre score est de : " + Array.from(new Set(sameNumber)).length);
   return sameNumber;
 }
-
-
 
 function createListOfNumber() {
   let listOfNum = [];
   for (let i = 0; i < 10; i++) {
-    let rnd = Math.floor(Math.random() * 7 + 1);
+    let rnd = Math.floor(Math.random() * 9 + 1);
     listOfNum.push(rnd);
   }
   return listOfNum;
@@ -37,12 +33,9 @@ function displayResult() {
 
 function play() {
   let entries = document.getElementById("user-inputnum").value;
-  entries = Array.from(String(Math.abs(entries)), Number)
-// Math.absolute to ensure that we are working with a positive number even if it is negative 
-// the second arg of from map all element in the first arg to number
-
-  listComparison(entries,createListOfNumber())
-
-
+  entries = Array.from(String(Math.abs(entries)), Number);
+  // Math.absolute to ensure that we are working with a positive number even if it is negative
+  // the second arg of from map all element in the first arg to number
+  listComparison(entries, createListOfNumber());
   displayResult();
 }
